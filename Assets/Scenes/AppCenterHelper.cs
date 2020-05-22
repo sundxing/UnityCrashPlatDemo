da1c82d6384070bf2ef4e31914c3cc2ba6fc9225
duanxing.sun <duanxing.sun@minifoxgame.com>
2020-05-22 11:40:29 +0800

    Attach local logs

--- Assets/Scenes/AppCenterHelper.cs
+++ Assets/Scenes/AppCenterHelper.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
 using Microsoft.AppCenter.Unity.Crashes;
 using UnityEngine;
 
@@ -7,31 +8,91 @@ namespace MF.Art.Game
 {
     public class AppCenterHelper : MonoBehaviour
     {
-        
-        [DllImport("google_breakpad")]
-        private static extern void setupNativeCrashesListener(string path);
 
+        private static string LogPath;
+        public static string GetCrashLogPath()
+        {
+            return LogPath;
+        }
+        
         private static void BindNDKHandler()
         {
 #if UNITY_ANDROID && !UNITY_EDITOR
-              var minidumpDir = Crashes.GetMinidumpDirectoryAsync();
-            setupNativeCrashesListener(minidumpDir.Result);
+            var minidumpDir = Crashes.GetMinidumpDirectoryAsync();
+            var path = minidumpDir.Result;
             
-             Debug.Log("####AppCenterHelper Init BindNDKHandler");
+            
+            var crashLogPath = GetCrashLogPath();
+            if (!File.Exists(crashLogPath)) {
+                File.Create(crashLogPath);
+            }
+
+            var jniLoaderClass = new AndroidJavaClass("com.example.libdumplog.JniLoader");
+
+            jniLoaderClass.CallStatic("InitNativeCrashListener",
+                new AndroidJavaObject("java.lang.String", path),
+                new AndroidJavaObject("java.lang.String", crashLogPath));
+
+            Debug.Log($"####AppCenterHelper Init BindNDKHandler {crashLogPath}");
 #endif
         
         }
 
-        private void Start()
+        private byte[] FileContent(string fileName)
         {
-            Invoke(nameof(Init), 2f);
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                try
+                {
+                    byte[] buffur = new byte[fs.Length];
+                    fs.Read(buffur, 0, (int)fs.Length);
+                    return buffur;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+        private void Awake()
+        {
+            LogPath = Application.persistentDataPath + "/crash_log.txt";
+            
+            AppCenterBehavior.Started += OnAppCenterStart;
+            
+            Crashes.GetErrorAttachments = report =>
+            {
+ #if !UNITY_ANDROID
+ 
+return null;
+#endif
+                var path = LogPath;
+                if (!File.Exists(path))
+                {
+                    return null;
+                }
+                
+                var result = new[]
+                {
+                    ErrorAttachmentLog.AttachmentWithText(File.ReadAllText(path), "CrashLog.txt"),
+                };
+                
+                File.Delete(path);
+
+                return result;
+            };
+        }
+
+        private void OnAppCenterStart()
+        {
+            Init();
         }
 
         public void Init()
         {
             Debug.Log("####AppCenterHelper Init");
             Crashes.SetEnabledAsync(true);
-            Crashes.ReportUnhandledExceptions(true);
+            Crashes.ReportUnhandledExceptions(true, true);
             BindNDKHandler();
         }
     }
